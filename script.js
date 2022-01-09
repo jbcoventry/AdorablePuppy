8f9c7c511bbd9dd31e86c3efa4d15dd812a54544
Jayden <jayden@jbcov.com>
2022-01-09 16:44:54 +1100

    Name maker done

--- script.js
+++ script.js
@@ -29,14 +29,38 @@ const adjectives = [
   "Sleepy",
   "Slimey",
 ];
+let randomAdjList = [];
+let randompetList = [];
+let randomCombined = [];
 
 let picker = (array) =>
-  array.splice(Math.floor(Math.random() * adjectives.length), 1);
+  array.splice(Math.floor(Math.random() * array.length), 1);
 
-let randomAdjList = [];
-for (let i = 0; i < petList.length - 1; i++) {
-  randomAdjList.push(picker(adjectives));
-}
+let numberGenerator = () => Math.floor(Math.random() * (30 - 10 + 1) + 10);
 
-picker(adjectives);
-let generatedPetNames = picker(petList);
+let listMaker = (inList, outList) => {
+  while (inList.length) {
+    outList.push(picker(inList));
+  }
+};
+
+listMaker(adjectives, randomAdjList);
+listMaker(petList, randompetList);
+
+let listCombiner = (firstList, secondList) => {
+  if (firstList.length <= secondList.length) {
+    randomCombined.push(
+      firstList.map((item, index) => item + " " + secondList[index])
+    );
+  } else {
+    randomCombined.push(
+      secondList.map((item, index) => firstList[index] + " " + item)
+    );
+  }
+};
+listCombiner(randomAdjList, randompetList);
+
+console.log(randomAdjList);
+console.log(randompetList);
+console.log(randomCombined);
+console.log(numberGenerator);
