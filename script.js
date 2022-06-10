aa655be645f33247ee2fbbdd58994bba099aa609
Jayden <jayden@jbcov.com>
2022-06-11 07:53:11 +1000

    killed counter

--- script.js
+++ script.js
@@ -1,143 +1,139 @@
 "use strict";
-
-let barAnimator = (parentDiv, childDiv, statMax) => {
-  let parent = document.getElementById(parentDiv);
-  let parentStyle = getComputedStyle(parent);
-  let parentWidth = parseInt(parentStyle.width);
-  const parentWidthPercent = parentWidth / 100;
-  let child = document.getElementById(childDiv);
-  let reducedBy = 0;
-
-  let barReducer = () => {
-    if (parentWidth > reducedBy) {
-      child.style.width = parentWidth - reducedBy + "px";
-      reducedBy += parentWidthPercent/ statMax;
-    } else {
-      child.style.width = 0;
-      clearInterval(reduceRepeater);
-    }
-  };
-
-  let reduceRepeater = setInterval(barReducer, 10);
-
-  parent.onclick = () => {
-    clearInterval(reduceRepeater);
-    reduceRepeater = setInterval(barReducer, 10);
-    child.style.width = parentWidth + "px";
-    reducedBy = 0;
-  };
-};
-let barMaker = (stat) => {
-  let statBarParent = document.createElement("div");
-  statBarParent.className = "barParent";
-  statBarParent.id = "statBarParent"+stat+counter;
-  document.getElementById("petDiv"+counter).append(statBarParent);
-  let statBarChild = document.createElement("div");
-  statBarChild.className = "barChild";
-  statBarChild.id = "statBarChild" + stat+ counter;
-  statBarParent.append(statBarChild);
-  }
-const petList = [
-  "Kitten",
-  "Whale",
-  "Tarantula",
-  "Emu",
-  "Worms",
-  "Hamster",
-  "Rock",
-  "Cow",
-  "Horse",
-  "Pig",
-  "Capybara",
+const minPossibleTraitValue = 5000;
+const maxPossibleTraitValue = 20000;
+const newPetRate = 10000;
+const animals = [
+    "Kitten",
+    "Whale",
+    "Tarantula",
+    "Emu",
+    "Worms",
+    "Hamster",
+    "Rock",
+    "Cow",
+    "Horse",
+    "Pig",
+    "Capybara",
+    "Puppy",
 ];
-
 const adjectives = [
-  "Fetching",
-  "Cute",
-  "Goofy",
-  "Cheeky",
-  "Rambunctious",
-  "Kindhearted",
-  "Huggable",
-  "Mischievous",
-  "Jolly",
-  "'Big Boned'",
-  "Tiny",
-  "Sleepy",
-  "Slimey",
+    "Fetching",
+    "Cute",
+    "Goofy",
+    "Cheeky",
+    "Rambunctious",
+    "Kindhearted",
+    "Huggable",
+    "Mischievous",
+    "Jolly",
+    "'Big Boned'",
+    "Tiny",
+    "Sleepy",
+    "Slimey",
+    "Adorable",
 ];
-let randomAdjList = [];
-let randompetList = [];
-let randomCombined = [];
-let pets = {};
 
-let picker = (array) =>
-  array.splice(Math.floor(Math.random() * array.length), 1);
 
-let numberGenerator = () => Math.floor(Math.random() * (10 - 3 + 1) + 3);
-
-let listMaker = (inList, outList) => {
-  while (inList.length) {
-    outList.push(picker(inList));
-  }
+const randomNumberBetween = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const createPetValues = (petName) => {
+    return {
+        name: petName,
+        traitFeed: randomNumberBetween(minPossibleTraitValue, maxPossibleTraitValue),
+        traitClean: randomNumberBetween(minPossibleTraitValue, maxPossibleTraitValue),
+        traitPlay: randomNumberBetween(minPossibleTraitValue, maxPossibleTraitValue),
+        traitTrain: randomNumberBetween(minPossibleTraitValue, maxPossibleTraitValue),
+    };
 };
 
-listMaker(adjectives, randomAdjList);
-listMaker(petList, randompetList);
+const shuffleArray = (toBeShuffled) =>
+    toBeShuffled
+        .map((value) => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value);
 
-let listCombiner = () => {
-  for (let i = 0; i < randomAdjList.length && i < randompetList.length; i++) {
-    randomCombined.push(randomAdjList[i] + " " + randompetList[i]);
-  }
+const adjectiveAnimalCombiner = (adjectives, animals) => {
+    return adjectives.slice(0, animals.length).map(function (value, index) {
+        return value + " " + animals[index];
+    });
 };
 
-listCombiner();
+const petsMaker = (adjectives, animals) => {
+    return adjectiveAnimalCombiner(shuffleArray(adjectives), shuffleArray(animals)).map(
+        createPetValues
+    );
+};
 
-class Pet {
-  constructor(name) {
-    this.name = name;
-    this.feedNeed = numberGenerator();
-    this.cleanNeed = numberGenerator();
-    this.petNeed = numberGenerator();
-    this.trainNeed = numberGenerator();
-    this.feedCount = this.feedNeed;
-    this.cleanCount = this.cleanNeed;
-    this.petCount = this.petNeed;
-    this.trainCount = this.feedNeed;
-  }
+const pets = petsMaker(adjectives, animals);
+const petDivMaker = (petsList, petsListPosition) =>{
+    const petDiv = document.createElement("div");
+    petDiv.className = "pet-div"
+    petDiv.id = `pet-div-${petsListPosition}`;
+    const petHeading = document.createElement("div");
+    petHeading.className = "pet-heading"
+    petHeading.id = `pet-heading-${petsListPosition}`;
+    petHeading.textContent = `${petsList[petsListPosition].name}`;
+    petDiv.append(petHeading);
+    petDiv.append(traitBarMaker("feed", petsListPosition));
+    petDiv.append(traitBarMaker("clean", petsListPosition));
+    petDiv.append(traitBarMaker("play", petsListPosition));
+    petDiv.append(traitBarMaker("train", petsListPosition));
+    return petDiv;
+}
+const traitBarMaker = (trait, petsListPosition) => {
+    const traitBarOuter = document.createElement("div");
+    traitBarOuter.className = "trait-bar-outer";
+    traitBarOuter.id = `trait-bar-outer-${trait}-${petsListPosition}`;
+    const traitBarInner = document.createElement("div");
+    traitBarInner.className = "trait-bar-inner";
+    traitBarInner.id = `trait-bar-inner-${trait}-${petsListPosition}`;
+    traitBarOuter.append(traitBarInner);
+    return traitBarOuter;
+};
+
+const barAnimator = (durationInMS, progressBarInnerDivID) => {
+    let starttime = null;
+    const maxWidth = document.getElementById(`${progressBarInnerDivID}`).parentElement.clientWidth;
+
+    function animate(timestamp) {
+        if (!starttime) {
+            starttime = timestamp;
+        }
+
+        const runtime = timestamp - starttime;
+        const relativeProgress = runtime / durationInMS;
+
+        document.getElementById(`${progressBarInnerDivID}`).style.width = `${
+            maxWidth - maxWidth * relativeProgress
+        }px`;
+        if (runtime < durationInMS) {
+            requestAnimationFrame(animate);
+        }
+    }
+
+    requestAnimationFrame(animate);
+};
+
+const newPetToDOM = () => {
+    const totalPets = document.getElementById("pet-pen").childElementCount;
+    document.getElementById("pet-pen").append(petDivMaker(pets,totalPets));
+    barAnimator(pets[totalPets].traitFeed,`trait-bar-inner-feed-${totalPets}`);
+    barAnimator(pets[totalPets].traitClean,`trait-bar-inner-clean-${totalPets}`);
+    barAnimator(pets[totalPets].traitPlay,`trait-bar-inner-play-${totalPets}`);
+    barAnimator(pets[totalPets].traitTrain,`trait-bar-inner-train-${totalPets}`);
+    setTimeout(newPetToDOM, newPetRate);
 }
 
-pets[1] = new Pet("Adorable Puppy");
-
-for (let i = 2; i < 6; i++) {
-  pets[i] = new Pet(randomCombined[i]);
-}
-
-let counter = 1;
-let start = () => {
-  let petDivVariable = document.createElement("div");
-  petDivVariable.className = "petDiv";
-  petDivVariable.id = "petDiv" + counter;
-  petDivVariable.innerHTML = pets[counter].name;
-  document.getElementById("petPen").append(petDivVariable);
-  barMaker("Feed");
-  barAnimator("statBarParentFeed"+counter,"statBarChildFeed"+counter,pets[counter].feedNeed);
-  barMaker("Clean");
-  barAnimator("statBarParentClean"+counter,"statBarChildClean"+counter,pets[counter].cleanNeed);
-  barMaker("Pet");
-  barAnimator("statBarParentPet"+counter,"statBarChildPet"+counter,pets[counter].petNeed);
-  barMaker("Train");
-  barAnimator("statBarParentTrain"+counter,"statBarChildTrain"+counter,pets[counter].trainNeed);
-
-  counter++;
-  if (counter > 5) {
-    clearInterval(intervalVariable);
-  }
-};
-
-let intervalVariable;
-let timedRelease = () => (intervalVariable = setInterval(start, 2000));
-
-startButton.addEventListener("click", timedRelease);
-startButton.addEventListener("click", startButton.remove);
-
+document.addEventListener("click", (event) =>{
+ if (event.target.matches("#start-button")){
+    newPetToDOM();
+    event.target.remove();
+ }
+ if (event.target.matches(".trait-bar-outer")){
+    event.target.firstChild.style.width = "100%";
+ }
+ if (event.target.matches(".trait-bar-inner")){
+    event.target.style.width = "100%";
+ }
+})
